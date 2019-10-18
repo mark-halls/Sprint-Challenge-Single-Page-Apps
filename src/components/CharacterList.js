88885aed4270b141933f519f92b7651665d2e75f
Mark Halls <mark.halls@gmail.com>
2019-10-18 08:58:52 -0700

    Get data from api and log to console

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,30 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characterData, setCharacterData] = useState();
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+      .get("https://rickandmortyapi.com/api/character/")
+      .then(res => {
+        console.log(res.data);
+        setCharacterData(res.data);
+      })
+      .catch(err => console.error(err));
   }, []);
 
+  if (!characterData) {
+    return <div>Loading...</div>;
+  }
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {characterData.results.map((character, index) => {
+        console.log(character);
+      })}
     </section>
   );
 }
