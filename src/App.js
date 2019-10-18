9d910d49b2467526e3036ea0dd55af6a6b7654ea
Mark Halls <mark.halls@gmail.com>
2019-10-18 09:39:37 -0700

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
+import { Route, Switch, NavLink } from "react-router-dom";
+
 import Header from "./components/Header.js";
 import CharacterList from "./components/CharacterList";
+import WelcomePage from "./components/WelcomePage";
 
 export default function App() {
   return (
     <main>
       <Header />
-      <CharacterList />
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/characters">Characters</NavLink>
+      <Switch>
+        <Route
+          path="/characters"
+          render={props => <CharacterList {...props} />}
+        />
+        <Route path="/" component={WelcomePage} />
+      </Switch>
     </main>
   );
 }
